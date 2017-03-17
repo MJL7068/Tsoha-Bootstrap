a3f72a03811d192f3b1d30835392d7dfd8ff62cb
MJL7068 <mattil@netti.fi>
2017-03-17 17:34:40 +0200

    sivuja lisätty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,15 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Opiskelija(
+    id SERIAL PRIMARY KEY,
+    opiskelijanumero varchar(50) NOT NULL,
+    nimi varchar(50) NOT NULL,
+    salasana varchar(50), NOT NULL
+);
+
+CREATE TABLE Aihe(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL,
+    kuvaus varchar(400),
+    vaikeustaso varchar(50) NOT NULL,
+    maksimiarvosana INTEGER NOT NULL
+);
