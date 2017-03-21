b952c25d1136b5915971f6a13085aa41e9db1411
MJL7068 <mattil@netti.fi>
2017-03-21 12:06:50 +0200

    sivuja muokattu & tietokantataulut lisätty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,15 +1,39 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
 CREATE TABLE Opiskelija(
     id SERIAL PRIMARY KEY,
-    opiskelijanumero varchar(50) NOT NULL,
     nimi varchar(50) NOT NULL,
-    salasana varchar(50), NOT NULL
+    opiskelijanumero varchar(50) NOT NULL
+);
+
+CREATE TABLE Opettaja(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Yllapitaja(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL,
+    salasana varchar(20) NOT NULL
+);
+
+CREATE TABLE Kurssi(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL
 );
 
 CREATE TABLE Aihe(
     id SERIAL PRIMARY KEY,
     nimi varchar(50) NOT NULL,
-    kuvaus varchar(400),
-    vaikeustaso varchar(50) NOT NULL,
-    maksimiarvosana INTEGER NOT NULL
+    vaikeustaso varchar(20) NOT NULL,
+    maksimiarvosana INTEGER NOT NULL,
+    kurssi INTEGER REFERENCES Kurssi(id),
+    kuvaus varchar(400)
+);
+
+CREATE TABLE Suoritus(
+    id SERIAL PRIMARY KEY,
+    aihe INTEGER REFERENCES Aihe(id),
+    tekija INTEGER REFERENCES Opiskelija(id),
+    ohjaaja INTEGER REFERENCES Opettaja(id),
+    tyomaara INTEGER,
+    arvosana INTEGER
 );
